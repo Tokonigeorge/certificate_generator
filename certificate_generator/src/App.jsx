70b71e3e206a9b585437971f8112fd054d7592bd
Bukola <bukolaebikefe@yahoo.com>
2021-10-19 01:36:10 +0100

    setup app and tailwind config

---
+++ certificate_generator/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p className="text-red-400">Test text</p>
+    </div>
+  );
+}
+
+export default App;
