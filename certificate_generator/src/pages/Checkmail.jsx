216d9de14b132a998bdf4abb8ea662dc2dbbeace
Bukola <bukolaebikefe@yahoo.com>
2021-10-20 10:13:25 +0100

    download cert flow

--- certificate_generator/src/pages/Checkmail.jsx
+++ certificate_generator/src/pages/Checkmail.jsx
@@ -1,22 +1,121 @@
-import React from "react";
+import React, { useState } from "react";
+import Input from "../components/input";
+import Button from "../components/button";
 
 const Checkmail = ({ verify }) => {
+  const message = "Oops. Email not registered, go back?";
+  const [value, setValue] = useState("");
+  const [hasDownload, setHasDownload] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
+
+  const btnData = {
+    actionType: "Download",
+    styles:
+      "bg-blue-400 px-4 py-2 text-white rounded-sm font-semibold hover:bg-opacity-90 w-full md:w-1/2",
+    handleClick: (e) => {
+      console.log(e);
+    },
+  };
+
+  const btnSubmitName = {
+    actionType: "Submit",
+    styles:
+      "bg-blue-400 px-4 py-2 text-white rounded-sm font-semibold hover:bg-opacity-90 w-full md:w-1/2",
+    handleClick: (e) => {
+      console.log(e);
+    },
+  };
+
+  const data = {
+    styles:
+      "ring-1 ring-blue-400 focus:outline-none focus:ring-1 focus:ring-black px-4 py-2 rounded-sm mb-4 w-full md:w-1/2",
+    placeholder: "Enter your full name",
+    htmlFor: "full-name",
+    type: "text",
+    value: value,
+    handleKeyDown: (e) => {
+      if (e.key === "Enter") {
+        e.preventDefault();
+      }
+    },
+    handleChange: (e) => {
+      setValue(e.target.value);
+    },
+  };
+
   const Notfound = ({ message }) => {
     return (
-      <div>
-        <p>Ooops, {message}</p>
-      </div>
+      <>
+        <h2 className="text-4xl md:text-5xl font-semibold mb-4 px-4 text-center">
+          {message}
+        </h2>
+      </>
     );
   };
-  const Download = ({ hasDownloaded }) => {
+
+  const InputName = () => {
     return (
-      <div>
-        <p className="text-center text-lg mb-4 px-4"> Download here</p>
-        <a href="/">Or send to mail</a>
-      </div>
+      <>
+        <h2 className="text-4xl md:text-5xl font-semibold mb-4 px-4 text-center">
+          Enter Name
+        </h2>
+        <p className="text-center text-lg mb-4 px-4">
+          Please note this is how it will appear on the certificate and should
+          be crosschecked.
+        </p>
+        <form className="w-screen text-center px-4 md:pd-0">
+          <Input {...data} />
+          <Button {...btnSubmitName} />
+        </form>
+      </>
     );
   };
-  return <div>{message ? <Notfound message={message} /> : <Download />}</div>;
+
+  const HasDownload = () => {
+    return (
+      <>
+        <h2 className="text-4xl md:text-5xl font-semibold mb-4 px-4 text-center">
+          You have already generated a certificate.
+        </h2>
+        <p className="text-center text-lg mb-4 px-4">Download again here</p>
+        <form className="w-screen text-center px-4 md:pd-0">
+          <Button {...btnData} />
+        </form>
+      </>
+    );
+  };
+
+  const DownloadCert = () => {
+    return (
+      <>
+        <h2 className="text-4xl md:text-5xl font-semibold mb-4 px-4">
+          Download Certificate here.
+        </h2>
+        <form className="w-screen text-center px-4 md:pd-0">
+          <Button {...btnData} />
+        </form>
+      </>
+    );
+  };
+
+  const Download = () => {
+    return (
+      <>
+        {hasDownload ? (
+          <HasDownload />
+        ) : isClicked ? (
+          <DownloadCert />
+        ) : (
+          <InputName />
+        )}
+      </>
+    );
+  };
+  return (
+    <div className="container max-auto h-auto grid place-items-center items-center mt-48">
+      {verify ? <Notfound message={message} /> : <Download />}
+    </div>
+  );
 };
 
 export default Checkmail;
