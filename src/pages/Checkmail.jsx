9e6a6319adaf0cdee370337435408bc3210d3edb
Tokonigeorge <bukolaebikefe@yahoo.com>
2022-10-18 11:54:07 +0100

    text over image and pdf download, install firebase

--- src/pages/Checkmail.jsx
+++ src/pages/Checkmail.jsx
@@ -1,30 +1,73 @@
+import { saveAs } from 'file-saver'
 import React, { useState } from "react";
 import Input from "../components/input";
 import Button from "../components/button";
+import { Page, Text, View, Document, StyleSheet, pdf, Image, Font} from '@react-pdf/renderer';
+
+
 
 const Checkmail = ({ email, verify }) => {
   const message = "Oops. Email not registered, go back?";
   const [value, setValue] = useState("");
-  const [name, setName] = useState(null);
+  const [name, setName] = useState("AdeSumnola Omotola Ebikefe");
   const [isDownload, setIsDownload] = useState(false);
   const [isClicked, setIsClicked] = useState(false);
 
  
-  const getDownload = () => {
-    if (localStorage.getItem(email)) {
-      return true;
+  // const getDownload = () => {
+  //   if (localStorage.getItem(email)) {
+  //     return true;
+  //   }
+  // };
+// Font.register({ family: 'Courier', src: source });
+
+  const check = 500/name?.length  > 24
+  const styles = StyleSheet.create({
+  page: {
+
+  },
+  section: {
+
+    },
+    image: {
+      width: "100%",
+      height: `${check ? '125%': '125%'}`,
+    },
+    text: {
+      fontFamily: 'Courier',
+      fontStyle: 'normal',
+      fontSize: `${ check ? '24px':'16px'}`,
+      marginTop: `${check ? '-31.8%': '-30.5%'}`,
+      marginLeft: '28.5%',
+      width: `500px`,
+      textAlign: 'center',
     }
-  };
+  });
+  
+  const MyDoc = () => (
+  <Document>
+    <Page size="A4" style={styles.page} orientation="landscape" wrap={false}>
+      <View>
+          <Image src="../src/assets/certificate.jpg" style={styles.image} />
+          <Text style={styles.text} fixed={true}>AdeSumnola Omotola Ebikefe</Text>
+        </View>
+    </Page>
+  </Document>
+);
 
   const btnData = {
     actionType: "Download",
     styles:
       "bg-blue-400 px-4 py-2 text-white rounded-sm font-semibold hover:bg-opacity-90 w-full md:w-1/2",
-    handleClick: (e) => {
+    handleClick: async (e) => {
       e.preventDefault();
-      if (!localStorage.getItem(email)) {
-        localStorage.setItem(email, value);
-      }
+      // if (!localStorage.getItem(email)) {
+      //   localStorage.setItem(email, value);
+      // }
+       const blob = await pdf(
+        <MyDoc />
+    ).toBlob();
+    saveAs(blob, 'wycena.pdf');
       setIsDownload(!isDownload);
     },
   };
@@ -93,9 +136,9 @@ const Checkmail = ({ email, verify }) => {
   };
 
   const HasDownload = () => {
-    if (localStorage.getItem(email)) {
-      setName(localStorage.getItem(email));
-    }
+    // if (localStorage.getItem(email)) {
+    //   setName(localStorage.getItem(email));
+    // }
     return (
       <>
         <h2 className="text-4xl md:text-5xl font-semibold mb-4 px-4 text-center">
@@ -116,6 +159,11 @@ const Checkmail = ({ email, verify }) => {
           Download Certificate here.
         </h2>
         <form className="w-screen text-center px-4 md:pd-0">
+           {/* <PDFDownloadLink document={<MyDoc />} fileName="somename.pdf">
+      {({ blob, url, loading, error }) =>
+        loading ? 'Loading document...' : 'Download Document'
+      }
+    </PDFDownloadLink> */}
           <Button {...btnData} />
         </form>
       </>
@@ -125,11 +173,12 @@ const Checkmail = ({ email, verify }) => {
   return (
     <>
       {isDownload ? (
-        <div className="container relative">
+        <div className="container relative bg-red-400 h-screen w-screen">
           <img
             src="../src/assets/certificate.jpg"
             alt="certificate"
-            className=""
+            className="object-contain w-screen h-screen"
+
           />
           <div className="text-center absolute top-1/2 left-1/3">
             {name || value}
@@ -137,10 +186,20 @@ const Checkmail = ({ email, verify }) => {
         </div>
       ) : (
         <div className="container max-auto h-auto grid place-items-center items-center mt-48">
-          {verify ? (
+          {/* {verify ? (
             getDownload() ? (
               <HasDownload />
             ) : isClicked ? (
+              <DownloadCert />
+            ) : (
+              //called as a function to prevent rerendering of input and loosing focus
+              inputName()
+            )
+          ) : (
+            <Notfound message={message} />
+          )} */}
+            {verify ? (
+            isClicked ? (
               <DownloadCert />
             ) : (
               //called as a function to prevent rerendering of input and loosing focus
